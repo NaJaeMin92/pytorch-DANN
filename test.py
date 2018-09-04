3e7dd3569d5bfcaf1f5ba04ad5088cd3a799f01c
NaJaeMin92 <37066691+NaJaeMin92@users.noreply.github.com>
2018-09-04 22:04:41 +0900

    Create test.py

---
+++ test.py
@@ -0,0 +1,71 @@
+import torch
+from torch.autograd import Variable
+import numpy as np
+
+
+def tester(encoder, classifier, discriminator, source_test_loader, target_test_loader, training_mode):
+    print("Model test ...")
+
+    encoder.cuda()
+    classifier.cuda()
+
+    if training_mode == 'dann':
+        discriminator.cuda()
+        domain_correct = 0
+
+    source_correct = 0
+    target_correct = 0
+
+    for batch_idx, (source_data, target_data) in enumerate(zip(source_test_loader, target_test_loader)):
+        p = float(batch_idx) / len(source_test_loader) 
+        alpha = 2. / (1. + np.exp(-10 * p)) - 1
+
+        # 1. Source input -> Source Classification
+        source_image, source_label = source_data
+        source_image, source_label = Variable(source_image.cuda()), Variable(source_label.cuda())
+        # source_domain_labels = Variable(torch.zeros((source_image.size()[0])).type(torch.LongTensor).cuda())
+        source_image = torch.cat((source_image, source_image, source_image), 1)  # MNIST convert to 3 channel
+        source_feature, _ = encoder(source_image)
+        source_output = classifier(source_feature)
+        source_pred = source_output.data.max(1, keepdim=True)[1]
+        source_correct += source_pred.eq(source_label.data.view_as(source_pred)).cpu().sum()
+
+        # 2. Target input -> Target Classification
+        target_image, target_label = target_data
+        target_image, target_label = Variable(target_image.cuda()), Variable(target_label.cuda())
+        # target_domain_labels = Variable(torch.zeros((target_image.size()[1])).type(torch.LongTensor).cuda())
+        target_feature, _ = encoder(target_image)
+        target_output = classifier(target_feature)
+        target_pred = target_output.data.max(1, keepdim=True)[1]
+        target_correct += target_pred.eq(target_label.data.view_as(target_pred)).cpu().sum()
+
+        if training_mode == 'dann':
+            # 3. Combined input -> Domain Classificaion
+            combined_image = torch.cat((source_image, target_image), 0)  # 64 = (S:32 + T:32)
+            domain_source_labels = torch.zeros(source_label.shape[0]).type(torch.LongTensor)
+            domain_target_labels = torch.ones(target_label.shape[0]).type(torch.LongTensor)
+            domain_combined_label = torch.cat((domain_source_labels, domain_target_labels), 0).cuda()
+            domain_feature, _ \
+                = encoder(combined_image)
+            domain_output = discriminator(domain_feature, alpha)
+            domain_pred = domain_output.data.max(1, keepdim=True)[1]
+            domain_correct += domain_pred.eq(domain_combined_label.data.view_as(domain_pred)).cpu().sum()
+
+    if training_mode == 'dann':
+        print("Test Results on DANN :")
+        print('\nSource Accuracy: {}/{} ({:.2f}%)\n'
+              'Target Accuracy: {}/{} ({:.2f}%)\n'
+              'Domain Accuracy: {}/{} ({:.2f}%)\n'.
+            format(
+            source_correct, len(source_test_loader.dataset), 100. * source_correct / len(source_test_loader.dataset),
+            target_correct, len(target_test_loader.dataset), 100. * target_correct / len(target_test_loader.dataset),
+            domain_correct, len(source_test_loader.dataset) + len(target_test_loader.dataset), 100. * domain_correct / (
+                                                                         len(source_test_loader.dataset) + len(
+                                                                     target_test_loader.dataset))
+        ))
+    else:
+        print("Test results on source_only :")
+        print('\nSource Accuracy: {}/{} ({:.2f}%)\n'
+              'Target Accuracy: {}/{} ({:.2f}%)\n'.format(
+            source_correct, len(source_test_loader.dataset), 100. * source_correct / len(source_test_loader.dataset),
+            target_correct, len(target_test_loader.dataset), 100. * target_correct / len(target_test_loader.dataset)))
